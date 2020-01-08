56e713d16fe20dde7809016ed7f42d88b9b76301
Diego Sanchez <diegosanchztorres@gmail.com>
2020-01-07 19:21:01 -0600

    Update Guess_the_number.py

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -1,9 +1,10 @@
 import random
 SecretNumber=random.randrange(0,1000)
+win=False
 Name=input('What is your name?: ')
 print('Hello '+Name+', im thinking on an integer between 0 and 1000, take a guess:')
 for GuessesTaken in range(1,11):
-    #print('DEBUG:'+str(SecretNumber))
+    print('DEBUG:'+str(SecretNumber))
     print('Tries left:'+str(11-GuessesTaken))
     while True:
         try:
@@ -18,9 +19,11 @@ for GuessesTaken in range(1,11):
     elif int(Guess)>SecretNumber:
         print('Too high')
     else:
+        win=True
         break
-if GuessesTaken==10:
+if GuessesTaken==10 and not win:
     print('You ran out of tries')
+    print('The secret number was '+str(SecretNumber))
 else:
     print('That is the number, congrats!')
 
